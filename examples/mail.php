c87cd0ebb6e5990e49d38e163c71375f0d8c23ba
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2016-05-03 00:37:49 -0700

    Update mail.php

--- examples/mail.php
+++ examples/mail.php
@@ -4,5 +4,5 @@ This is the text content, but you can still use php to get a <?= $name ?>
 ---- html ----
 <h1>This is the HTML Content</h1>
 <p>
-	Again, you can use php to get a <? $name ?> and other such things.
+	Again, you can use php to get a <?= $name ?> and other such things.
 </p>
