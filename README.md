af47a970f58a9a5c931c7f69a8e340cbc4995820
Matthew J. Sahagian <msahagian@dotink.org>
2016-05-03 00:33:58 -0700

    README and examples

---
+++ README.md
@@ -0,0 +1,3 @@
+# Franklin
+
+Franklin is a simple bulk mailer that uses mailgun to send templated e-mails to simple CSV lists.
---
+++ examples/mail.php
@@ -0,0 +1,8 @@
+This is the subject of the mail
+---- text ----
+This is the text content, but you can still use php to get a <?= $name ?>
+---- html ----
+<h1>This is the HTML Content</h1>
+<p>
+	Again, you can use php to get a <? $name ?> and other such things.
+</p>
